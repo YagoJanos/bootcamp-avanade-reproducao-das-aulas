c8d771dfb40d5163517ed2c5a91a5b6615bce1b7
YagoJanos <yjquaranta@gmail.com>
2022-02-04 09:17:01 -0300

    Unit tests implementation

---
+++ Instalacao-Configuracao/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instalacao_Configuracao
+{
+    public class Conta
+    {
+        public double Soma(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtracao(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ Instalacao-ConfiguracaoTests/ContaTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Instalacao_Configuracao;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instalacao_Configuracao.Tests
+{
+    [TestClass()]
+    public class ContaTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            // arrange
+            var x = 5.0;
+            var y = 6.0;
+            var esperado = 11.0;
+            var teste = new Conta();
+
+            // act
+            var resultado = teste.Soma(x, y);
+
+            // assert
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            // arrange
+            var x = 5.0;
+            var y = 6.0;
+            var esperado = -1.0;
+            var teste = new Conta();
+
+            // act
+            var resultado = teste.Subtracao(x, y);
+
+            // assert
+            Assert.AreEqual(esperado, resultado);
+
+        }
+    }
+}
