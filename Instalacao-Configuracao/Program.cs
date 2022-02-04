8096a132deef6ff32bd6772d87ab8f426b3e5c56
YagoJanos <yjquaranta@gmail.com>
2022-02-03 21:16:07 -0300

    Adicionar arquivos de projeto.

---
+++ Instalacao-Configuracao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Instalacao_Configuracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            var b = 4;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
